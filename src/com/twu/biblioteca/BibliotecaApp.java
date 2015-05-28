e15304e2d7ba2aa52e832fd89bb5dadc9ddf0dc7
fanfal <271299626@qq.com>
2015-05-28 16:33:43 +0800

    [xuhao]:add the quit function

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -8,6 +8,10 @@ public class BibliotecaApp
   {
     System.out.println("Welcome to the Bangalore Public Library!");
     mainMenu();
+
+  }
+  public static void chooseTheOption()
+  {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     try
     {
@@ -17,19 +21,22 @@ public class BibliotecaApp
         case "L":
         bookDatebase();
         break;
+        case "Q":
+        mainMenu();
+        default:
+        System.out.println("Select a valid option");
       }
     }catch(IOException e)
     {
       e.printStackTrace();
     }
-
-
-
   }
   public static void mainMenu()
   {
-    System.out.println("Main Menu");
-    System.out.println("[L]listBooks");
+    System.out.print("[L]listBooks    ");
+    System.out.print("[Q]quit    ");
+    System.out.println("");
+    chooseTheOption();
 
   }
   public static void bookDatebase()
@@ -37,6 +44,7 @@ public class BibliotecaApp
     listBooks("老人与海","海明威",1951);
     listBooks("简爱","夏洛蒂～勃朗特",1847);
     listBooks("战争与和平","列夫～托尔斯泰",1965);
+    mainMenu();
   }
   public static void listBooks(String name,String author,int year)
   {
