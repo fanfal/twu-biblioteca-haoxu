68b0aee3dda041d7b6357762d3f51401fcea6277
fanfal <271299626@qq.com>
2015-05-28 15:45:10 +0800

    [xuhao]:add the Book Details and List Books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,5 +5,17 @@ public class BibliotecaApp
   public static void main(String[] args)
   {
     System.out.println("Welcome to the Bangalore Public Library!");
+    bookDatebase();
+
+  }
+  public static void bookDatebase()
+  {
+    listBooks("老人与海","海明威",1951);
+    listBooks("简爱","夏洛蒂～勃朗特",1847);
+    listBooks("战争与和平","列夫～托尔斯泰",1965);
+  }
+  public static void listBooks(String name,String author,int year)
+  {
+    System.out.println("Name:" + name + "     Author:" + author + "     Year:"+year);
   }
 }
