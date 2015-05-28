664180043eb7f0a5761503c3fd3545c8a135bb06
fanfal <271299626@qq.com>
2015-05-28 16:20:27 +0800

    [xuhao]:add the mainMenu function

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,11 +1,35 @@
 //package com.twu.biblioteca;
-
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 public class BibliotecaApp
 {
   public static void main(String[] args)
   {
     System.out.println("Welcome to the Bangalore Public Library!");
-    bookDatebase();
+    mainMenu();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    try
+    {
+      String option = br.readLine();
+      switch(option)
+      {
+        case "L":
+        bookDatebase();
+        break;
+      }
+    }catch(IOException e)
+    {
+      e.printStackTrace();
+    }
+
+
+
+  }
+  public static void mainMenu()
+  {
+    System.out.println("Main Menu");
+    System.out.println("[L]listBooks");
 
   }
   public static void bookDatebase()
